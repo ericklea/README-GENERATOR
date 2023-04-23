7dc4a49d0e2d559cd9d511399fcbed3fcf0b79f9
L E <erickson.leah.j@gmail.com>
2023-04-23 14:17:44 -0600

    re-structured index.js file

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,7 +1,58 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
+ //run inquirer prompt
+ const inquirer = require('inquirer');
+ const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [];
+//inquirer prompt
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Describe what your application does, the technologies you used, and the challanges you faced?',
+    },
+    {
+        type: 'checkbox',
+        name: 'Table of Contents',
+        message: ['Installation', 'Usage', 'Credits', 'Liscense'],
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide instruction and examples for use:',
+    },
+    {
+        type: 'input',
+        name: 'Credits',
+        message: 'List collaborators, tutorials followed, and materials used:',           
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'Indicate the licenses you used:',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+    },
+
+])
+ //write then response and console.log it
+ .then((response) => {
+    console.log(response);
+    const filename = `${response.Title.toLowerCase().split(' ').join('')}.json`;
+    fs.writeFile(filename, JSON.stringify(response, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+ });
+
+
+    console.log('Answers:', answers);
+    writeReadme(answers.Title, answers.Description, answers.TableOfContents, answers.Installation, answers.Usage, answers.License);
+
 
 //function to write README file
 function writeReadme(Title, Description, TableOfContents, Installation, Usage, License) {
@@ -36,59 +87,5 @@ function init() {
         writeReadme(answers.Title, answers.Description, answers.TableOfContents, answers.Installation, answers.Usage, answers.License);
     });
 }
-
-
-    //run inquirer prompt
-    const inquirer = require('inquirer');
-    const fs = require('fs');
-    //inquirer prompt
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'Description',
-            message: 'Describe what your application does, the technologies you used, and the challanges you faced?',
-        },
-        {
-            type: 'checkbox',
-            name: 'Table of Contents',
-            message: ['Installation', 'Usage', 'Credits', 'Liscense'],
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: 'What are the steps required to install your project?',
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message: 'Provide instruction and examples for use:',
-        },
-        {
-            type: 'input',
-            name: 'Credits',
-            message: 'List collaborators, tutorials followed, and materials used:',           
-        },
-        {
-            type: 'list',
-            name: 'License',
-            message: 'Indicate the licenses you used:',
-            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        },
-
-    ])
-    //write then response and console.log it
-    .then((response) => {
-        console.log(response);
-        const filename = `${response.Title.toLowerCase().split(' ').join('')}.json`;
-        fs.writeFile(filename, JSON.stringify(response, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('Success!')
-        );
-     });
-
-
-            //console.log('Answers:', answers);
-
-
-
 // Function call to initialize app
-init();
+init()
