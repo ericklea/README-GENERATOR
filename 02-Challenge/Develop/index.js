69ea6aca466bc73dd70e610bd86efa1fb0282d7d
L E <erickson.leah.j@gmail.com>
2023-04-23 12:07:20 -0600

    input usage info

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -6,53 +6,53 @@ const questions = [];
 // TODO: Create a function to write README file
 const fs = require('fs');
 
-function writeReadme(Title, Description, TableOfContents, Installation, Usage, License){
+function writeReadme(Title, Description, TableOfContents, Installation, Usage, License) {
 
 }
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
     //run inquirer prompt
     const inquirer = require('inquirer');
-//inquirer prompt
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'Description',
-    message: 'Describe what your application does, the technologies you used, and the challanges you faced?',
-  },
-  {
-    type: 'checkbox',
-    name: 'Table of Contents',
-    message: ['Installation','Usage','Credits','Liscense'],
-  },
-  {
-    type: 'input',
-    name: 'Installation',
-    message: 'What are the steps required to install your project?',
-},
-{
-  type: '',
-  name: 'Table of Contents',
-  message: ['Installation','Usage','Credits','Liscense'],
-},
-{
-  type: 'input',
-  name: 'Installation',
-  message: 'What are the steps required to install your project?',
+    //inquirer prompt
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Describe what your application does, the technologies you used, and the challanges you faced?',
+        },
+        {
+            type: 'checkbox',
+            name: 'Table of Contents',
+            message: ['Installation', 'Usage', 'Credits', 'Liscense'],
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'What are the steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Provide instruction and examples for use:',
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'What are the steps required to install your project?',
 
-    type: 'confirm',
-    name: 'confirm',
-    message: 'Are you sure?'
-  }
-])
-.then(answers => {
-  console.log('Answers:', answers);
-})
-.catch(error => {
-  console.log('Error:', error);
-});
+            type: 'confirm',
+            name: 'confirm',
+            message: 'Are you sure?'
+        }
+    ])
+        .then(answers => {
+            console.log('Answers:', answers);
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
 }
 //write then response and console.log it
 
