a2df48bec8a2fe6d90bee852dd94d67ae8f3b51a
L E <erickson.leah.j@gmail.com>
2023-04-23 11:11:22 -0600

    input inquirer prompt

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -14,6 +14,32 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
     //run inquirer prompt
+    const inquirer = require('inquirer');
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your name?"
+  },
+  {
+    type: 'list',
+    name: 'color',
+    message: 'What is your favorite color?',
+    choices: ['Red', 'Blue', 'Green', 'Yellow']
+  },
+  {
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Are you sure?'
+  }
+])
+.then(answers => {
+  console.log('Answers:', answers);
+})
+.catch(error => {
+  console.log('Error:', error);
+});
 }
 //write then response and console.log it
 
