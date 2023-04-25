e8b87a30c94585e158f03f52c7d8f85fa63b1014
L E <erickson.leah.j@gmail.com>
2023-04-25 15:28:39 -0600

    input generate markdown const

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -3,7 +3,7 @@
     const inquirer = require('inquirer');
     const fs = require('fs');
     const path = require('path');
-    
+    const generateMarkdown = require('./utils/generateMarkdown.js');
 // Array of questions for user input
 const questions = [
 
